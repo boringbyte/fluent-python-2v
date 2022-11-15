7343fd1ea32152d2b77def3a980b98caaa48c1d9
boringbyte <kottapallisivaram@gmail.com>
2022-11-15 09:00:01 -0500

    Finally, reading Fluent Python book

---
+++ Chapter09_DecoratorsAndClosures/AllExamples.py
@@ -0,0 +1,14 @@
+def deco(func):
+    def inner():
+        print('running inner()')
+    return inner
+
+
+@deco
+def target():
+    print('running target()')
+
+
+if __name__ == '__main__':
+    target()
+    print(target)
